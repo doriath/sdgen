a623e0a10769eb5a04a65cc88691682a2b1f517b
Tomasz Zurkowski <doriath88@gmail.com>
2011-05-05 18:48:02 +0200

    Add Loop.

--- svg.py
+++ svg.py
@@ -120,10 +120,18 @@ class Text(object):
 
 
 
+class Cardinality(object):
+  def __init__(self, content, data, conf):
+    self.padding = conf.default.padding
+    self.content = content
+    self.text = Text(data["cardinality"], conf.default.font)
+    self.width = content.width
+    self.height = content.height + self.text.height + self.padding
+    self.connect_y = content.connect_y + self.text.height + self.padding
 
-
-
-
+  def render(self, svg, x, y):
+    self.text.render(svg, x + self.content.width / 2 - self.text.width / 2, y)
+    self.content.render(svg, x, y + self.text.height + self.padding)
 
 
 
@@ -132,19 +140,29 @@ class Text(object):
 
 
 def create_element(data, conf):
+  result = None
   if data["view"] == "Group":
-    return Group(data, conf)
+    result = Group(data, conf)
   if data["view"] == "Terminal":
-    return Terminal(data, conf)
+    result = Terminal(data, conf)
   if data["view"] == "InvTerminal":
-    return InvTerminal(data, conf)
+    result = InvTerminal(data, conf)
   if data["view"] == "Non Terminal":
-    return NonTerminal(data, conf)
+    result = NonTerminal(data, conf)
   if data["view"] == "Alternation":
-    return Alternation(data, conf)
+    result = Alternation(data, conf)
   if data["view"] == "Detour":
-    return Detour(data, conf)
-  raise "Unknow view"
+    result =  Detour(data, conf)
+  if data["view"] == "Loop":
+    result =  Loop(data, conf)
+
+  if result == None:
+    raise "Unknow view"
+
+  if "cardinality" in data:
+    return Cardinality(result, data, conf)
+
+  return result
 
 class SimpleArrows(object):
   def __init__(self, data, conf):
@@ -241,9 +259,10 @@ class GroupBody(object):
 
     # draw arrows
     shape_builder = ShapeBuilder()
-    l = shape_builder.createLine(x, y + self.height / 2, x + 10, y + self.height / 2, strokewidth = 3)
+    connect_y = y + self.padding + self.content.connect_y
+    l = shape_builder.createLine(x, connect_y, x + 10, connect_y, strokewidth = 3)
     svg.addElement(l)
-    l = shape_builder.createLine(x + self.content_width + 10, y + self.height / 2, x + self.content_width + 11, y + self.height / 2, strokewidth = 3)
+    l = shape_builder.createLine(x + self.content_width + 10, connect_y, x + self.content_width + 11, connect_y, strokewidth = 3)
     l._attributes['marker-end'] = 'url(#right-arrow)'
     svg.addElement(l)
 
@@ -290,20 +309,24 @@ class Group(SimpleArrows):
 class LinearLayout(object):
   def __init__(self, data, conf):
     self.width = 0
-    self.height = 0
+    self.height_above = 0
+    self.height_below = 0
     self.children = []
     for raw_child in data:
       child = create_element(raw_child, conf)
       self.width += child.width
-      self.height = max(self.height, child.height)
+      self.height_above = max(self.height_above, child.connect_y)
+      self.height_below = max(self.height_below, child.height - child.connect_y)
       self.children.append(child)
+    self.height = self.height_above + self.height_below
+    self.connect_y = self.height_above
 
   def render(self, svg, x, y):
     max_height = 0
     for child in self.children:
       max_height = max(max_height, child.height)
 
-    connect_y = y + max_height / 2
+    connect_y = y + self.connect_y
     for child in self.children:
       child.render(svg, x, connect_y - child.connect_y)
       x += child.width
@@ -323,18 +346,18 @@ class Alternation(object):
   def render(self, svg, x, y):
     shape_builder = ShapeBuilder()
     start_x = x
-    start_y = y + self.height / 2
+    start_y = y + self.connect_y
     end_x = x + self.content_width + 20
 
     x += 20
     for child in [self.top, self.bottom]:
       child.render(svg, x, y)
 
-      l = shape_builder.createLine(x + child.width, y + child.height / 2, x + self.content_width, y + child.height / 2, strokewidth = 3)
+      l = shape_builder.createLine(x + child.width, y + child.connect_y, x + self.content_width, y + child.connect_y, strokewidth = 3)
       svg.addElement(l)
-      path_data = "m {0},{1} c {2},0 0,{3} {2},{3}".format(start_x, start_y, 20, y + child.height / 2 - start_y)
+      path_data = "m {0},{1} c {2},0 0,{3} {2},{3}".format(start_x, start_y, 20, y + child.connect_y - start_y)
       svg.addElement(path(path_data, stroke = "black", fill = "none", stroke_width = 3))
-      path_data = "m {0},{1} c {2},0 0,{3} {2},{3}".format(end_x, y + child.height / 2, 20, start_y - (y + child.height / 2))
+      path_data = "m {0},{1} c {2},0 0,{3} {2},{3}".format(end_x, y + child.connect_y, 20, start_y - (y + child.connect_y))
       svg.addElement(path(path_data, stroke = "black", fill = "none", stroke_width = 3))
 
       y += child.height + self.padding
@@ -362,6 +385,34 @@ class Detour(object):
     l._attributes['marker-end'] = 'url(#right-arrow)'
     svg.addElement(l)
     l = shape_builder.createLine(x + 20, bottom_y, x + 20 + self.content.width, bottom_y, strokewidth = 3)
+    svg.addElement(l)
+    l = shape_builder.createLine(x, connect_y, x + 20, connect_y, strokewidth = 3)
+    svg.addElement(l)
+    l = shape_builder.createLine(x + 20 + self.content.width, connect_y, x + 20 + self.content.width + 20, connect_y, strokewidth = 3)
+    svg.addElement(l)
+
+class Loop(object):
+  def __init__(self, data, conf):
+    self.content = LinearLayout(data["children"], conf)
+    self.width = self.content.width + 40
+    self.height = self.content.height + 20
+    self.connect_y = self.content.connect_y + 20
+
+  def render(self, svg, x, y):
+    self.content.render(svg, x + 20, y + 20)
+
+    connect_y = y + self.connect_y
+    above_y = connect_y - self.content.height_above - 10
+    path_data = "m {0},{1} c -10,0 -10,{3} 0,{3}".format(x + 20, connect_y, 20, above_y - connect_y)
+    svg.addElement(path(path_data, stroke = "black", fill = "none", stroke_width = 3))
+    path_data = "m {0},{1} c 10,0 10,{3} 0,{3}".format(x + 20 + self.content.width, above_y, 20, connect_y - above_y)
+    svg.addElement(path(path_data, stroke = "black", fill = "none", stroke_width = 3))
+
+    shape_builder = ShapeBuilder()
+    l = shape_builder.createLine(x + 20 + self.content.width / 2, above_y, x + 19 + self.content.width / 2, above_y, strokewidth = 3)
+    l._attributes['marker-end'] = 'url(#right-arrow)'
+    svg.addElement(l)
+    l = shape_builder.createLine(x + 20, above_y, x + 20 + self.content.width, above_y, strokewidth = 3)
     svg.addElement(l)
     l = shape_builder.createLine(x, connect_y, x + 20, connect_y, strokewidth = 3)
     svg.addElement(l)
