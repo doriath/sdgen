082d945d3dbf295bc74b0d7d6eea98cc6c0d2790
Piotr Slatala <piotr@sepio.pl>
2011-05-07 17:48:26 +0200

    FIX: Working with Polish letters

--- svg.py
+++ svg.py
@@ -6,6 +6,53 @@ from pysvg.shape import *
 from pysvg.builders import *
 import json
 
+def getXML(self):
+    """
+    Return a XML representation of the current element.
+    This function can be used for debugging purposes. It is also used by getXML in SVG
+
+    @return:  the representation of the current element as an xml string
+    """
+    xml=u'<'+unicode(self._elementName)+u' '
+    for key,value in self._attributes.items():
+        if value != None:
+            xml+=unicode(key)+'="'+self.quote_attrib(unicode(str(value)))+u'" '
+    if  len(self._subElements)==0: #self._textContent==None and
+        xml+=u' />\n'
+    else:
+        xml+=u' >\n'
+        #if self._textContent==None:
+        for subelement in self._subElements:
+            xml+=unicode(subelement.getXML())
+        #else:
+        #if self._textContent!=None:
+        #    xml+=self._textContent
+        xml+=u'</'+unicode(self._elementName)+u'>\n'
+    #print xml
+    return xml
+def wrap_xml(self, xml, encoding ='ISO-8859-1', standalone='no'):
+    """
+    Method that provides a standard svg header string for a file
+    """
+    header = u'''<?xml version="1.0" encoding="%s" standalone="%s"?>''' %(encoding, standalone)
+    header = header.encode("utf-8")
+    return header+xml
+
+def save(self, filename, encoding ='ISO-8859-1', standalone='no'):
+    """
+    Stores any element in a svg file (including header). 
+    Calling this method only makes sense if the root element is an svg elemnt
+    """
+    f = open(filename, 'w')
+    xml = self.getXML()
+    xml = xml.encode("utf-8")
+    f.write(self.wrap_xml(xml, encoding, standalone))
+    f.close()
+BaseElement.getXML = getXML
+BaseElement.wrap_xml = wrap_xml
+BaseElement.save = save
+
+
 def as_svg(data, path = None, conf = None):
   '''
   Generate a svg image(s). This function can generate multiple images.
@@ -446,7 +493,7 @@ def create_diagram(data, output, conf):
   diagram.render_content(s, 0, 0)
 
   # print s.getXML()
-  s.save(output)
+  s.save(output, 'utf-8')
 
 def main():
   conf = json.loads(open('conf.example').read())
